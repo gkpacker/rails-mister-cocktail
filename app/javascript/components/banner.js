2f5388d79d78da7b2eaa20ef7f0f21214df956d3
Gabriel Packer <ggkpacker@gmail.com>
2018-03-30 19:26:50 -0300

    bug fix

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,15 +1,16 @@
 import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
-  if (document.querySelector('#banner-typed-text'))
-  new Typed('#banner-typed-text', {
-    strings: ["Vodka", "Lemon", "Sugar", "Best cocktail ever"],
-    typeSpeed: 80,
-    smartBackspace: true,
-    backSpeed: 60,
-    backDelay: 700,
-    loop: true
-  });
+  if (document.querySelector('#banner-typed-text')) {
+    new Typed('#banner-typed-text', {
+      strings: ["Vodka", "Lemon", "Sugar", "Best cocktail ever."],
+      typeSpeed: 80,
+      smartBackspace: true,
+      backSpeed: 60,
+      backDelay: 700,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
