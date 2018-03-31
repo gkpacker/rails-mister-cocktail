e02671dae87c0e1c527f5d1bb365b2a4a230c21f
Gabriel Packer <ggkpacker@gmail.com>
2018-03-31 14:04:01 -0300

    query only in one delete btn fixed

--- app/javascript/components/alert.js
+++ app/javascript/components/alert.js
@@ -1,30 +1,33 @@
 import swal from 'sweetalert';
 
 function bindDeleteButton() {
-  document.getElementById('btn-delete').addEventListener('click', (event) => {
-    event.preventDefault();
-    // event.stopPropagation();
-    swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this dose!",
-      icon: "warning",
-      buttons: true,
-      dangerMode: true,
-    })
-    .then((willDelete) => {
-      if (willDelete) {
-        const btn = document.querySelector('#btn-delete');
-        // const url = btn
-        swal("Poof! Your dose has been deleted!", {
-          icon: "success",
-        });
-        $.ajax({
-          type: "DELETE",
-          url: btn.dataset["url"],
-        });
-      } else {
-        swal("Your dose is safe!");
-      }
+  const buttons = document.querySelectorAll(".btn-delete")
+  buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this dose!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          const btn = document.querySelector('#btn-delete');
+          const url = btn
+          swal("Poof! Your dose has been deleted!", {
+            icon: "success",
+          });
+          $.ajax({
+            type: "DELETE",
+            url: btn.dataset["url"],
+          });
+        } else {
+          swal("Your dose is safe!");
+        }
+      });
     });
   });
 }
